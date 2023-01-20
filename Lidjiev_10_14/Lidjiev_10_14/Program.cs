a042839521be98926a49183c00dbd7c68685a9ee
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2023-01-20 08:17:44 +0300

    Add files via upload

---
+++ Lidjiev_10_14/Lidjiev_10_14/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace lidjiev_10_14
+{
+    class Program
+    {
+
+        static int func(int[] array, int n)
+        {
+            int i, count = 0;
+            for (i = 0; i < n; ++i)
+            {
+                int k = 1;
+                do
+                {
+                    if (array[i] % 5 == 0 && array[i] != 0)
+                    {
+                        array[i] /= 5;
+                        k = 1;
+                    }
+                    else
+                    {
+                        k = 0;
+                        break;
+                    }
+                } while (array[i] != 1);
+                if (k == 1)
+                    count++;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            const int n = 100;
+            int[] array = new int[n];
+
+            Console.Write("Массив:");
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = rnd.Next(1000);
+                Console.Write(" " + array[i]);
+            }
+
+            Console.WriteLine("\nКол-во чисел, которые являются степенью пятерки " + func(array, n));
+
+        }
+    }
+}
